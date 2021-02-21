0b75517123415ea949d105cf9c9bb26c30022e76
Anthony Rojo <0xantman@Anthonys-MacBook-Pro.local>
2021-02-21 15:43:51 +0100

    work in progress

--- pages/public-sale.vue
+++ pages/public-sale.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-    <vs-card>
-      <template #title>
-        <h3>Public sale</h3>
-        <p>In construction...</p>
-      </template>
-      <template #text>
-        Soon a public sale will take place here. Stay tuned
-      </template>
-    </vs-card>
+  <div class="center row">
+    <vs-row justify="center">
+      <vs-col lg="5" sm="12" xs="12">
+        <vs-card>
+          <template #title>
+            <h3>LOTA</h3>
+            <p>LOTA is the token of LoTerra</p>
+          </template>
+          <template #text>
+            <ul>
+              <li>Supply: 7 000 000</li>
+              <li>price per LOTA: 1 UST</li>
+              <li>Hard cap: 7 000 000 $</li>
+              <li>Cw-20 token on Terra Luna</li>
+            </ul>
+            <p>
+              Unsold LOTA token will remain locked forever on the smart contract
+              after the public sale end.
+            </p>
+          </template>
+        </vs-card>
+      </vs-col>
+      <vs-col lg="2" sm="12" xs="12"></vs-col>
+      <vs-col lg="5" sm="12" xs="12"
+        ><vs-card>
+          <template #title>
+            <h3>Public sale</h3>
+            <p>Buy LOTA with UST and contribute to the jackpot Lottery</p>
+          </template>
+          <template #text>
+            When you buy LOTA your UST will go to the jackpot.
+          </template>
+        </vs-card>
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
@@ -18,4 +43,13 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+ul li {
+  margin: 10px;
+  text-align: left;
+}
+</style>
