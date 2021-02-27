3caa7d1949a1c929d931e357e74a943d75a3aeed
0xantman <antho74830@gmail.com>
2021-02-27 19:49:15 +0100

    add jackpot reward info

--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,116 @@
 <template>
   <div class="content-container">
     <LoTerraGame />
-    <div class="row" style="width: 100%; margin-top: 50px">
-      <!--<vs-row justify="space-around">
-        <vs-col lg="2" sm="12">
-          <vs-card>
-            <template #title>
-              <h3>Winner 6 symbols #rank1</h3>
-              <p></p>
-            </template>
-            <template #text>
-              <div style="display: flex; flex-direction: column">
-                <div>terra1umd70q...dxd46p</div>
-                <div>terra1umd70q...dxd46p</div>
+    <div
+      style="
+        margin-top: 50px;
+        padding: 25px;
+        background-color: white;
+        margin: 25px;
+        border-radius: 50px;
+      "
+    >
+      <div>
+        <h3 class="jackpot-winner-reward">Last jackpot rewards</h3>
+        <p>Potential rewards per rank</p>
+        <div>
+          <span class="jackpot-winner-reward">{{ jackpotFormat }}UST</span>
+        </div>
+        <div
+          style="
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+            flex-direction: column;
+          "
+        >
+          <div style="display: flex; align-items: center">
+            <vs-button flat danger> #Rank1 </vs-button> Prize 6 symbols:
+            <span class="jackpot-winner-reward">{{ prizeRank1 }}UST</span>
+          </div>
+          <div style="display: flex; align-items: center">
+            <vs-button flat danger> #Rank2 </vs-button> Prize 5 symbols:
+            <span class="jackpot-winner-reward">{{ prizeRank2 }}UST</span>
+          </div>
+          <div style="display: flex; align-items: center">
+            <vs-button flat danger> #Rank3 </vs-button> Prize 4 symbols:
+            <span class="jackpot-winner-reward">{{ prizeRank3 }}UST</span>
+          </div>
+          <div style="display: flex; align-items: center">
+            <vs-button flat danger> #Rank4 </vs-button> Prize 3 symbols:
+            <span class="jackpot-winner-reward">{{ prizeRank4 }}UST</span>
+          </div>
+        </div>
+      </div>
+      <h3 class="jackpot-winner-reward">Last winners</h3>
+      <div style="display: flex">
+        <div v-for="(item, index) in allWinners.winner" :key="index">
+          <div v-if="item.rank > 0 && item.rank < 5">
+            <div
+              style="
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              "
+            >
+              <div class="jackpot-winner-reward">#Rank{{ item.rank }}</div>
+              <div class="jackpot-winner-reward">
+                {{ item.winners.length }}
+                {{ item.winners.length == 1 ? 'winner' : 'winners' }}
               </div>
-            </template>
-          </vs-card>
-        </vs-col>
-        <vs-col lg="2" sm="12">
-          <vs-card>
-            <template #title>
-              <h3>Winner 5 symbols #rank2</h3>
-              <p></p>
-            </template>
-            <template #text>
-              <div style="display: flex; flex-direction: column">
-                <div>terra1umd70q...dxd46p</div>
-                <div>terra1umd70q...dxd46p</div>
-              </div>
-            </template>
-          </vs-card>
-        </vs-col>
-        <vs-col lg="2" sm="12">
-          <vs-card>
-            <template #title>
-              <h3>Winner 4 symbols #rank3</h3>
-              <p></p>
-            </template>
-            <template #text>
-              <div style="display: flex; flex-direction: column">
-                <div>terra1umd70q...dxd46p</div>
-                <div>terra1umd70q...dxd46p</div>
-              </div>
-            </template>
-          </vs-card>
-        </vs-col>
-        <vs-col lg="2" sm="12">
-          <vs-card>
-            <template #title>
-              <h3>Winner 3 symbols #rank4</h3>
-              <p></p>
-            </template>
-            <template #text>
-              <div style="display: flex; flex-direction: column">
-                <div>terra1umd70q...dxd46p</div>
-                <div>terra1umd70q...dxd46p</div>
-              </div>
-            </template>
-          </vs-card>
-        </vs-col>
-      </vs-row>-->
+              <span>🥳</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { LCDClient, WasmAPI } from '@terra-money/terra.js'
+import numeral from 'numeral'
 import LoTerraGame from '~/components/LoTerraGame'
-// eslint-disable-next-line no-unused-vars
 
 export default {
   components: {
     LoTerraGame,
   },
-  // middleware: 'terraConnect',
-  mounted() {
-    this.loadTicketPrice()
+  data: () => ({
+    allWinners: [],
+    jackpotTotalReward: '0',
+    prizePerRank: [],
+  }),
+  computed: {
+    jackpotFormat() {
+      return numeral(this.jackpotTotalReward).format('0,0.00')
+    },
+    prizeRank1() {
+      return numeral(
+        (this.prizePerRank[0] * parseInt(this.jackpotTotalReward)) / 100
+      ).format('0,0.00')
+    },
+    prizeRank2() {
+      return numeral(
+        (this.prizePerRank[1] * parseInt(this.jackpotTotalReward)) / 100
+      ).format('0,0.00')
+    },
+    prizeRank3() {
+      return numeral(
+        (this.prizePerRank[2] * parseInt(this.jackpotTotalReward)) / 100
+      ).format('0,0.00')
+    },
+    prizeRank4() {
+      return numeral(
+        (this.prizePerRank[3] * parseInt(this.jackpotTotalReward)) / 100
+      ).format('0,0.00')
+    },
   },
+  created() {
+    this.loadTicketPrice()
+    this.loadWinners()
+  },
+  // middleware: 'terraConnect',
   methods: {
     loadTicketPrice() {
       const client = new LCDClient({
@@ -97,6 +131,25 @@ export default {
             'station/update_ticket_price',
             config.price_per_ticket_to_register
           )
+          this.jackpotTotalReward = parseInt(config.jackpot_reward) / 1000000
+          this.prizePerRank = config.prize_rank_winner_percentage
+        })
+    },
+    loadWinners() {
+      const client = new LCDClient({
+        URL: this.$store.state.station.lcdUrl,
+        chainID: this.$store.state.station.lcdChainId,
+      })
+      const api = new WasmAPI(client.apiRequester)
+      api
+        .contractQuery(
+          this.$store.state.station.loterraLotteryContractAddress,
+          {
+            winner: {},
+          }
+        )
+        .then((winners) => {
+          this.allWinners = winners
         })
     },
     loadWallet: () => {
@@ -117,5 +170,12 @@ export default {
 .subtitle a {
   font-weight: 500;
   color: inherit;
+}
+.jackpot-winner-reward {
+  background: linear-gradient(to right, rgb(242, 19, 93), #5b3cc4 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 1.5rem;
+  padding: 10px;
 }
 </style>
