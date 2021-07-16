80bad90e2e24fd1028f0ed13307926afcdf20f69
0xantman <antho74830@gmail.com>
2021-07-16 19:53:13 +0200

    Claimed manually

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,10 @@
           </template>
           <template #tbody>
             <vs-tr v-for="(winner, key) in allWinners.winners" :key="key">
-              <vs-td> {{ winner.claims.claimed }}</vs-td>
+              <vs-td>
+                <!--{{ winner.claims.claimed }}-->
+                true</vs-td
+              >
               <vs-td>
                 {{ winner.claims.ranks }}
               </vs-td>
